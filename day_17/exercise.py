67974197271834a4580ecedde66d20597bdc6f9f
hugofriasmtz <a3521110393@alumno.uttehuacan.edu.mx>
2025-12-27 21:01:42 -0600

    :sparkles: feat: day seventeen python challenge

---
+++ day_17/exercise.py
@@ -0,0 +1,7 @@
+countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+*nordic_countries, es, ru = countries
+
+print(nordic_countries)  # ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+print(es)                # Estonia
+print(ru)                # Russia
